3442c6d1ceeb57fbb4905127c41ce1232d4fe12c
robu <robert.mitchell.iii@gmail.com>
2019-09-10 00:02:27 -0400

    Fix package configuration to actually include files.

--- setup.py
+++ setup.py
@@ -1,3 +1,7 @@
 from setuptools import setup, find_packages
 
-setup(name="haikoo", version="1.0.0", packages=find_packages())
+setup(
+	name="haikoo",
+	version="1.0.0",
+	packages=find_packages(include=["."])
+)
